e180cf622471dc0a486c2418226f8d177af43e9e
BusinessDuck <narkozzforb@gmail.com>
2025-01-27 19:23:06 +0100

    new address

--- packages/lottery-core/src/constants.ts
+++ packages/lottery-core/src/constants.ts
@@ -1,6 +1,6 @@
 // TODO: Need to be moved to outside to json file and used as dynamic import
 export const IS_TESTNET = false;
-export const CONTRACT_ADDRESS = 'EQD_MZ4D8UAbwFC4tCtLi98mM8op2tHtUcVA8gsRMqN7t69K';
+export const CONTRACT_ADDRESS = 'EQBblIEc4oJWLbjZibzbxOyD8VySyDX_sXOCfXcL8ExGm4nG';
 export const TON_CLIENT_API_KEY = '2c437893fc3f19d3f6d682f1822937770c44c851c366a20d49b9e35b5d518e0d';
 export const TON_CLIENT_ENDPOINT = IS_TESTNET
   ? 'https://testnet.toncenter.com/api/v2/jsonRPC'
